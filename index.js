dd6581402b280449daef9db1f03901c85501c457
Aaron Byrom <aaron.byr@gmail.com>
2021-11-23 13:12:43 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,54 +1,22 @@
+const form = document.querySelector('#form');
+const list = document.querySelector('#list');
 
-//searches GitHub for users by name and displays the results on the screen
+const getData = async (url) => {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+}
 
-//Using the results of the search, display information about the users
+const createHtml = (username, imageSource) => {
+  return `
+      <a href="${username}>
+        <img src="${imageSource}" alt="" />
+      </a>
+   `;
+}
 
-//You might include showing their username, avatar and a link to their profile
-
-//Clicking on a specific user will show all the repositories for that user
-
-//Toggle the search bar between searching for users by keyword and searching 
-//for repos by keyword by adding an extra button
-//(https://developer.github.com/v3/search/#search-repositories)
-
-
-const form = document.getElementById("form");
-const button = document.getElementById("button");
-
-form.addEventListener('submit' , function(e){
-    e.preventDefault()
-
-//get value
-const search = document.getElementById("search").value 
-//console.log(value)
-
-const originalName = search.split(' ').join('')
-console.log(originalName)
-
-// Search API 
-fetch("https://api.github.com/users").data
-console.log(data)
-
-
-const searchGithub = async () => {
-   try {
-    //#user-search-endpoint
-    const result = await fetch("https://api.github.com/users" + originalName);//
-    const data =  await result.json();
-    console.log(data);
-} catch (err) {
-    console.error(err);
- 
-document.getElementById("result").innerHTML = `
-<a target ="_blank" href="https://www.github.com/${originalName}" <img src = "${data.avatar_url} "/></a>
-`
-}; 
-} 
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const serverResponse = await getData(PUT_THE_GITHUB_URL_HERE_I_DONT_REMEMBER_IT);
+  list.innerHTML = createHtml(serverResponse.WHATEVER_THE_OBJECT_NOTATION_SHOULD_BE);
 })
-
-
-
-
-
-
-//search.addEventListener('input' , () => searchList(search.value)
